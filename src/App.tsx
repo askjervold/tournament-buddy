cb71cbe60e07d6e7beb6a890b8540a832154847b
Audun Skjervold <audun@skjervold.me>
2021-10-04 22:35:47 +0200

    Add router and initial containers

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import './App.css';
+import Routes from './constants/routes';
+import Home from './containers/home';
+import Tournament from './containers/tournament';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path={Routes.HOME}>
+          <Home />
+        </Route>
+        <Route path={Routes.TOURNAMENT}>
+          <Tournament />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/containers/tournament.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Tournament() {
+  return (
+    <button>Add player</button>
+  );
+}
+
+export default Tournament;
