c4fb2d74254119c72edd150f86cb1a3b3cf83181
Audun Skjervold <audun@skjervold.me>
2021-10-07 23:17:15 +0200

    Generate random seatings for draft

--- src/containers/draft.tsx
+++ src/containers/draft.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import shuffle from 'lodash/shuffle';
+
 import { useTournamentContext } from '../contexts/tournament-context';
+import Routes from '../constants/routes';
 
 function Draft() {
   const { players } = useTournamentContext();
+  const seats = shuffle(players)
+    .map((player, index) => ({ player, seat: index + 1 }));
 
   return (
     <>
       <h1>Seatings</h1>
       <section className="draft-table">
-        {players.map(player => <div className="seat">{player.name}</div>)}
+        {seats.map(seat =>
+          <div key={seat.seat} className="seat">{seat.seat}. {seat.player.name}</div>
+        )}
       </section>
+      <Link to={Routes.TOURNAMENT}>Back to overview</Link>
     </>
   );
 }
