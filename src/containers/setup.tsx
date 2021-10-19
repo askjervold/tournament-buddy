d8df0dfb498f53427ec157b2da72349fdb3d863d
Audun Skjervold <audun@skjervold.me>
2021-10-19 21:17:07 +0200

    Improve tournament setup UX

--- src/containers/setup.tsx
+++ src/containers/setup.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import Button from '../components/button';
 import Routes from '../constants/routes';
@@ -10,12 +10,18 @@ export default function Setup() {
   const { players, setPlayers, startTournament } = useTournamentContext();
   const [name, setName] = useState<string>('');
   const [editing, setEditing] = useState<number>(-1);
+  const addPlayerRef = useRef<HTMLInputElement>(null);
+
+  const handleInputKeypress = (event: React.KeyboardEvent) => {
+    if (event.key === 'Enter') addPlayer();
+  }
 
   const addPlayer = () => {
     setPlayers((prev) => {
       return [...prev, { name, id: players.length + 1 }];
     });
     setName('');
+    addPlayerRef?.current?.focus();
   };
 
   const removePlayer = (index: number) => {
@@ -51,12 +57,6 @@ export default function Setup() {
   return (
     <>
       <h1>Set up tournament</h1>
-      <input
-        type="text"
-        value={name}
-        onChange={(event) => setName(event.currentTarget.value)}
-      />
-      <Button onClick={addPlayer} inline={true}>Add player</Button>
       <ol className="player-list">
         {players.map((player, index) => (
           <li className="player" key={'player' + player.id}>
@@ -84,8 +84,24 @@ export default function Setup() {
           </li>
         ))}
       </ol>
-      <Button onClick={start}>Start tournament</Button>
-      <Button onClick={cancel} remove={true}>Cancel tournament</Button>
+
+      <div className="add-player">
+        <input
+          ref={addPlayerRef}
+          type="text"
+          value={name}
+          onChange={(event) => setName(event.currentTarget.value)}
+          onKeyPress={handleInputKeypress}
+        />
+        <Button onClick={addPlayer} inline={true}>Add player</Button>
+      </div>
+
+      <div className="btn-group">
+        <Button onClick={cancel} inline={true} remove={true}>Cancel tournament</Button>
+        {players.length > 0 &&
+          <Button onClick={start} inline={true}>Start tournament</Button>
+        }
+      </div>
     </>
   );
 }
