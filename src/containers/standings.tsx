ccf040c9d1728bb4fcbd91263f264b3f72de25bf
Audun Skjervold <audun@skjervold.me>
2022-02-21 20:09:05 +0100

    Style standings table

--- src/containers/standings.tsx
+++ src/containers/standings.tsx
@@ -19,30 +19,43 @@ function Standings() {
 
   return rounds.length > 0 ? (
     <>
-      <h1>Standings after {rounds.length} round{rounds.length > 1 ? 's' : ''}</h1>
+      <h1>
+        Standings after {rounds.length} round{rounds.length > 1 ? 's' : ''}
+      </h1>
       <table className="standings">
         <thead>
           <tr>
             <th>Place</th>
             <th>Player</th>
             <th>Score</th>
-            <th className="tiebreaker">OMW%</th>
-            <th className="tiebreaker">GW%</th>
-            <th className="tiebreaker">OGW%</th>
+            <th className="tiebreaker sm">OMW%</th>
+            <th className="tiebreaker sm">GW%</th>
+            <th className="tiebreaker sm">OGW%</th>
             <th className="tiebreaker mobile">Tiebreakers</th>
           </tr>
         </thead>
         <tbody>
-        {standings.map((player, index) => (
-          <tr key={player.player.id}>
-            <td>{index + 1}</td>
-            <td>{player.player.name}</td>
-            <td>{getScore(player.record)}</td>
-            <td className="tiebreaker">{formatPercentage(player.opponentMatchWin)}</td>
-            <td className="tiebreaker">{formatPercentage(player.gameWin)}</td>
-            <td className="tiebreaker">{formatPercentage(player.opponentGameWin)}</td>
-          </tr>
-        ))}
+          {standings.map((player, index) => (
+            <tr key={player.player.id}>
+              <td>{index + 1}</td>
+              <td>{player.player.name}</td>
+              <td>{getScore(player.record)}</td>
+              <td className="tiebreaker sm">
+                {formatPercentage(player.opponentMatchWin)}
+              </td>
+              <td className="tiebreaker sm">
+                {formatPercentage(player.gameWin)}
+              </td>
+              <td className="tiebreaker sm">
+                {formatPercentage(player.opponentGameWin)}
+              </td>
+              <td className="tiebreaker mobile">
+                {formatPercentage(player.opponentMatchWin)}/
+                {formatPercentage(player.gameWin)}/
+                {formatPercentage(player.opponentGameWin)}
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
       <LinkButton to={Routes.TOURNAMENT}>Back to overview</LinkButton>
